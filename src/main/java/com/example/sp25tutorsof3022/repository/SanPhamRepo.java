061c7959e5064c6c981ecb6f8880b169287c2503
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2025-02-20 12:59:48 +0700

    updtae

--- src/main/java/com/example/sp25tutorsof3022/repository/SanPhamRepo.java
+++ src/main/java/com/example/sp25tutorsof3022/repository/SanPhamRepo.java
@@ -2,8 +2,14 @@ package com.example.sp25tutorsof3022.repository;
 
 import com.example.sp25tutorsof3022.model.SanPham;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SanPhamRepo extends JpaRepository<SanPham, Integer> {
+
+    @Query(value = "select * from san_pham where ten_san_pham = :tenSp And trang_thai = :trangThai", nativeQuery = true)
+    List<SanPham> timKiemTheoTenVaTrangThai(String tenSp, String trangThai);
 }
